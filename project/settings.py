c24c5889539932ed213b6540581acdeb2872d651
Letícia Leal Moreira <lealmleticia@gmail.com>
2022-10-07 13:11:01 -0300

    feature: atualização dos componentes

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "django-insecure-y*2py)1khxv^q)!ze18tt$rigr$rn8n@eqe4s#jml=2$auv@56"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
